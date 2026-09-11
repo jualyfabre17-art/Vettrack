95205fb909ced513ed08c7383d377d0b7f891596
Jualy <jualyfabre17@gmail.com>
2026-09-11 07:55:03 -0400

    Continuando de configurar los repositorios

---
+++ Vettrack/Repositorios/IDueno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vettrack.Duenos;
+
+namespace Vettrack.Repositorios
+{
+    public class IDueno: IRepositorio <Dueno>
+    {
+        private List <Dueno> dueno = new List<Dueno> ();
+        public void Agregar(Dueno entidad) { }
+        public List<Dueno> ObtenerTodos() { return dueno; }
+        public Dueno ObtenerPorId(int id) { return null; }
+        public void Actualizar(Dueno entidad) { }
+        public void Eliminar(int id) { }
+    }
+}
---
+++ Vettrack/Repositorios/IMascota.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vettrack.Mascotas;
+using Vettrack.Repositorios;
+
+namespace Vettrack.Repositorios
+{
+    public class RepositorioMascota: IRepositorio<Mascota>
+    {
+        private List <Mascota> mascota = new List<Mascota>();
+        public void Agregar(Mascota entidad) { }
+        public Mascota ObtenerPorId(int id) { return null; }
+        public List<Mascota> ObtenerTodos() { return mascota; }
+        public void Actualizar(Mascota entidad) { }
+        public void Eliminar(int id) { }
+    }
+}
