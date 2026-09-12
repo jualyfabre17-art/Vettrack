9b0a38f20b6e2905565ff456099d265bd717b0e1
Jualy <jualyfabre17@gmail.com>
2026-09-12 08:24:58 -0400

    Reestructuracion del proyecto a razor pages para continuar con la pagina web, todos los datos, archivos fueron traspasados de manera segura y se alojaron correctamente en el nuevo proyecto

--- Vettrack/Program.cs
+++ Vettrack/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+namespace Vettrack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
