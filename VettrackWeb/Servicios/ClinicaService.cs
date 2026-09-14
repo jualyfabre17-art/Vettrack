e946104b89f37c3496585c7b8898173f7d720cd1
Jualy <jualyfabre17@gmail.com>
2026-09-14 10:43:15 -0400

    Iniciando con las dependencias de la clase service

---
+++ VettrackWeb/Servicios/ClinicaService.cs
@@ -0,0 +1,24 @@
+﻿using Vettrack.Repositorios;
+using Vettrack.Mascotas;
+using Vettrack.Duenos;
+using Vettrack.Veterinarios;
+using Vettrack.Citas;
+
+namespace VettrackWeb.Servicios
+{
+    public class ClinicaService
+    {
+        private readonly IRepositorio<Mascota> _repoMascota;
+        private readonly IRepositorio<Dueno> _repoDueno;
+        private readonly IRepositorio<Veterinario> _repoVeterinario;
+        private readonly IRepositorio<Cita> _repoCita;
+
+        public ClinicaService(IRepositorio<Mascota> repoMascota, IRepositorio<Dueno> repoDueno, IRepositorio<Veterinario> repoVeterinairo, IRepositorio<Cita> repoCita) 
+        {
+            _repoMascota = repoMascota;
+            _repoDueno = repoDueno;
+            _repoVeterinario = repoVeterinairo;
+            _repoCita = repoCita;
+        }
+    }
+}
